7b032568dd64f1c6440a036468f78d78d3c6458d
Jakob Work <jakob.langseth@gmail.com>
2021-07-06 18:09:12 -0700

    updated comments

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+// Requirements
 const program = require('commander')
 const chalk = require("chalk")
 const clippy = require("clipboardy")
 const createPassword = require("./utils/createPassword.js")
 const savePassword = require("./utils/savePassword.js")
 
+// Set options for users to see
 program.version('1.0.0').description('Password Generator')
 
+
+// Set options
 program
 .option('-l --length <length>', "Length of password", '8')
 .option('-s --save', "Save password to password.txt")
@@ -13,17 +17,16 @@ program
 .option('-ns --no-symbols', "Remove symbols")
 .parse()
 
+// Set options as constants
 const { length, save, numbers, symbols } = (program.opts())
 
 // Get generated password
 const generatedPassword = createPassword(length, numbers, symbols)
 
-// Save to file
-
+// Save to file if the save flag is set
 if(save) {
     savePassword(generatedPassword)
 }
-
 
 // Copy to clipboard
 clippy.writeSync(generatedPassword)
--- utils/createPassword.js
+++ utils/createPassword.js
@@ -1,17 +1,22 @@
+// Requirements
 const alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 const numbers = '0123456789'
 const symbols = '!@#$%^&*()_-+='
 
+// Create password function, P.S. default values are redundantly set here as well.
 const createPassword = (length = 8 , hasNumbers = true, hasSymbols = true) => {
     let chars = alpha
     hasNumbers ? (chars += numbers) : ""
     hasSymbols ? (chars += symbols) : ""
+    // Generate password based on what symbols, numbers and characters are available
     return generatePassword(length, chars)
 }
 
 const generatePassword = (length, chars) => {
     let password = ""
+    // P.S. length is the length of the password
     for(let i = 0; i < length; i++) {
+        // Randomize the selection of a character in the total available characters
         password += chars.charAt(Math.floor(Math.random() * chars.length))
     }
     return password
